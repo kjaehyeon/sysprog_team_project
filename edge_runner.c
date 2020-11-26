4919c44f9d6496b733412d081f499809c324f54a
kjaehyeon <jacob9404@gmail.com>
2020-11-26 20:18:52 +0900

    edge_runner.c and header

---
+++ edge_runner.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <curses.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include "edge_runner.h"
+enum state { start, in_play, help};//enum for store program state
+
+void helpWin();
+int play();
+
+int main(void){
+	char ch;
+	int play_retVal = 0; 
+	enum state cur_state = start;
+	
+	int flag = fcntl(0, F_GETFL);
+	flag |= O_NONBLOCK;
+	fcntl(0, F_SETFL, flag);
+
+	initscr();
+	crmode();
+	noecho();
+	
+	startWin();
+
+	while(1){
+		ch = getch();
+		
+		if(ch == 'h' &&	cur_state == start){
+			helpWin();
+			cur_state = help;
+		}
+		if(ch == 'p' && cur_state == start){	
+			cur_state = in_play;
+			play();
+			endwin();
+		//	play_retVal = play();
+
+		/*	if(play_retVal == 1){
+				cur_state = start;
+				startWin();
+			}else exit(0);*/
+			
+		}
+		if(ch == 'b' && cur_state == help){
+			startWin();
+			cur_state = start;
+		}
+		
+		//test code from here
+		if(ch == 'b' && cur_state == in_play){
+			startWin();
+			cur_state = start;
+		}
+		if(ch == 'e' && cur_state == start){
+			endwin();
+			printf("exit\n");
+			exit(0);
+		}
+
+	}
+}
---
+++ edge_runner.h
@@ -0,0 +1,5 @@
+void startWin();
+int gameover(int);
+int play();
+void helpWin();
+
