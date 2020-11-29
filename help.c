8f1295482e66bab819e16ffb5a29dea4c3937949
jungu12 <kjg070146@gmail.com>
2020-11-29 20:00:58 +0900

    help

--- help.c
+++ help.c
@@ -1,34 +1,53 @@
 #include<stdio.h>
 #include<curses.h>
 #include<string.h>
-
-void helpWin(){
-	
-	clear();
+int main(){
+	initscr();
 	int c;
-	move(0,0);
+	move(0,COLS-30);
 	addstr("press 'b' to go startpage");
-	move(10,COLS/2-30);
+	move(2,COLS/2-30);
 	addstr("#     # ####### #       ###### ");
-	move(11,COLS/2-30);
+	move(3,COLS/2-30);
 	addstr("#     # #       #       #     # ");
-	move(12,COLS/2-30);
+	move(4,COLS/2-30);
 	addstr("####### #####   #       ###### ");
-	move(13,COLS/2-30);
+	move(5,COLS/2-30);
 	addstr("#     # #       #       #     ");
-	move(14,COLS/2-30);
+	move(6,COLS/2-30);
 	addstr("#     # ####### ####### # ");
-	move(23,0);
+	move(23,40);
 	addstr("Your character should survive running through edge of console.");
-	move(24,0);
+	move(24,50);
 	addstr("Obstacles and Monsters are randomly spawn.");
-	move(25,0);
-	addstr("*Jump - space bar");
-	move(26,0);
-	addstr("*Attack - a");
-	move(27,0);
-	addstr("*Slide - s");
-
+	move(30,COLS/2-35);
+	addstr(" * ");
+	move(31,COLS/2-35);
+	addstr("***");
+	move(32,COLS/2-35);
+	addstr(" | "); 
+	move(31,COLS/2-30);
+	addstr("Jump -press 'space bar' when you meet tree");
+	move(35,COLS/2-36);
+	addstr(" o o ");
+	move(36,COLS/2-36);
+	addstr("-(@)-");
+	move(37,COLS/2-36);
+	addstr(" | | ");
+	move(36,COLS/2-29);
+	addstr("Attack - press 'a' when you meet monster");
+	move(40,COLS/2-35);
+	addstr("====");
+	move(41,COLS/2-35);
+	addstr("|  |");
+	move(42,COLS/2-35);
+	addstr("|  |");
+	move(43,COLS/2-35);
+	addstr("|  |");
+	move(41,COLS/2-30);
+	addstr("Slide - press 's' when you meet hurdle");
 	refresh();
+	getch();
+	endwin();
 
 }
